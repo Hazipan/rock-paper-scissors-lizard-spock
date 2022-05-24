0df3524cde4d3b3e101e7b9ea2f4e5154e418db7
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-05-24 13:30:14 -0700

    Added winning logic
    - The computer picks a symbol when you do
    - The to are compared and a result of win or lose is given
    - On win +1 point, on lose -1
    - Play again button added
    - Result is not visually acceptable. Next step is to create an animation to display results in an appealing way

--- src/App.js
+++ src/App.js
@@ -20,8 +20,14 @@ class App extends React.Component {
     super(props);
     this.state = {
       score: 0,
-      screen: 'app'
+      choiceMade: false,
+      playerChoice: '',
+      houseChoice: '',
+      win: 'default'
     }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.playAgain = this.playAgain.bind(this);
   }
 
   rulesClose() {
@@ -33,32 +39,118 @@ class App extends React.Component {
   }
 
   handleClick(event) {
-    console.log(event.target.value);
+    console.log(`${event.target.value} selected`);
+    // Generate number 1-5 to determine computer choice
+    console.log(Math.floor(Math.random() * 5));
+    const random = Math.floor(Math.random() * 5);
+    let pcChoice = '';
+    switch (random) {
+      case 1: pcChoice = 'rock'; break;
+      case 2: pcChoice = 'paper'; break;
+      case 3: pcChoice = 'scissors'; break;
+      case 4: pcChoice = 'lizard'; break;
+      default: pcChoice = 'spock';
+    }
+
+    // Check win conditions and change result acocordingly
+    const win = 'You Win!'
+    const lose = 'You Lose.'
+    let result = '';
+    let point = 1;
+    switch (event.target.value) {
+      case 'rock':
+        if(pcChoice === 'scissors' || pcChoice === 'lizard') {
+          result = win;
+        } else {
+          result = lose;
+          point = -1;
+        }
+        break;
+      case 'paper':
+        if(pcChoice === 'rock' || pcChoice === 'spock') {
+          result = win;
+        } else {
+          result = lose;
+          point = -1;
+        }
+        break;
+      case 'scissors':
+        if(pcChoice === 'paper' || pcChoice === 'lizard') {
+          result = win;
+        } else {
+          result = lose;
+          point = -1;
+        }
+        break;
+      case 'lizard':
+        if(pcChoice === 'paper' || pcChoice === 'spock') {
+          result = win;
+        } else {
+          result = lose;
+          point = -1;
+        }
+        break;
+      case 'spock':
+        if(pcChoice === 'scissors' || pcChoice === 'rock') {
+          result = win;
+        } else {
+          result = lose;
+          point = -1;
+        }
+        break;
+      default:
+        result = lose;
+        console.log(lose);
+    }
+
+    this.setState({
+      score: this.state.score + point,
+      choiceMade: true,
+      playerChoice: event.target.value,
+      houseChoice: pcChoice,
+      win: result
+    });
   }
 
+  playAgain() {
+    this.setState({choiceMade: false});
+  }
+    
+
   render() {
-    return (
-      <div className="App">
-        <header className='header' >
-          <img src={logo} alt='logo' className='logo' />
-          <Score value={this.state.score} />
-        </header>
-        <img src={pentagon} alt='' className='pentagon' />
-        <div className='pentaTop'>
-          <Button src={scissors} symbol='scissors' onClick={this.handleClick} />
+    if (this.state.choiceMade) {
+      return (
+        <div className="App">
+          <h1>You selected: {this.state.playerChoice}</h1>
+          <p>The house selected: {this.state.houseChoice}</p>
+          <p>{this.state.win}</p>
+          <button onClick={this.playAgain}>Play again!</button>
         </div>
-        <div className='pentaMiddle'>
-          <Button src={spock} symbol='spock' onClick={this.handleClick} />
-          <Button src={paper} symbol='paper' onClick={this.handleClick} />
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className='header' >
+            <img src={logo} alt='logo' className='logo' />
+            <Score value={this.state.score} />
+          </header>
+          <img src={pentagon} alt='' className='pentagon' />
+          <div className='pentaTop'>
+            <Button src={scissors} symbol='scissors' onClick={this.handleClick} />
+          </div>
+          <div className='pentaMiddle'>
+            <Button src={spock} symbol='spock' onClick={this.handleClick} />
+            <Button src={paper} symbol='paper' onClick={this.handleClick} />
+          </div>
+          <div className='pentaBottom'>
+            <Button src={lizard} symbol='lizard' onClick={this.handleClick} />
+            <Button src={rock} symbol='rock' onClick={this.handleClick} />
+          </div>
+          <Rules rules={rules} close={close} onClick={this.rulesClose} />
+          <button className='rulesButton' onClick={this.rulesOpen}>RULES</button>
         </div>
-        <div className='pentaBottom'>
-          <Button src={lizard} symbol='lizard' onClick={this.handleClick} />
-          <Button src={rock} symbol='rock' onClick={this.handleClick} />
-        </div>
-        <Rules rules={rules} close={close} onClick={this.rulesClose} />
-        <button className='rulesButton' onClick={this.rulesOpen}>RULES</button>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -2,13 +2,13 @@ import './rules.css';
 
 const Rules = (props) => {
     return(
-        <div className='rulesScreen' id='rulesScreen'>
+        <div className='rulesScreen hideRules' id='rulesScreen'>
             <div className='rulesCard'>
                 <div className='rulesHeader'>
                     <p>RULES</p>
-                    <img src={props.close} className='close' onClick={props.onClick} />
+                    <img src={props.close} className='close' onClick={props.onClick} alt='close' />
                 </div>
-                <img src={props.rules} className='rules' />
+                <img src={props.rules} className='rules' alt='rules' />
             </div>
         </div>
     )
